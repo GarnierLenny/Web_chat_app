6f6f160eff6d43f94b86fe0c3f1e857194650f2f
GarnierLenny <61317098+GarnierLenny@users.noreply.github.com>
2024-07-19 17:38:08 +0400

    FEAt - simple go server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello from server!")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", handleRoot)
+}
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
